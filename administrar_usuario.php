171826fece959180a423ede58c9aca75a722d125
Barsallo19 <jose.barsallo@utp.ac.pa>
2021-07-09 16:03:30 -0500

    agregado administrar_usuario

---
+++ administrar_usuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="front-end/administrar_usuario.css">
+    <title>Document</title>
+</head>
+<body>
+
+<center>
+    <h1>DySy Quest</h1>
+    <h1>administrar_usuario</h1>
+</center>
+
+<!-- !ESTA FUNCIONALIDAD TODAVÍA NO ESTA LISTA!
+nadie le haga cambios por favor que yo se los haré GRACIAS!!
+-->
+<div class="contenedor">
+<button class="boton" onclick="location.href='añadir_usuario.php'" type="button">Añadir Usuario</button>
+<button class="boton" onclick="location.href='eliminar_usuario.php'" type="button">Eliminar Usuario</button>
+<button class="boton2" onclick="location.href='vista.php'" type="button">Salir</button>
+            </div>
+</body>
+</html>
