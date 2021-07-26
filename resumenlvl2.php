956a4fc0d670f509f47ae46786533aa83145c42d
K3vinC <kevincoba04@gmail.com>
2021-07-25 22:09:51 -0500

    se agrego vistas de los resumenes

---
+++ resumenlvl2.php
@@ -0,0 +1,42 @@
+<?php
+        require('templates/header.php');
+?>
+
+<main class="form-signin"> <img class="mb-4 " src="front-end/imagenes/icono.png" alt="" width="100" height="80">
+    <div class="container ">
+        <div class=" m-4 position-relative " style="height:500px;">
+            <div class="bg-light rounded shadow border position-absolute top-50 start-50 translate-middle"
+            width="560" height="315">
+                <div class="card-header rounded-top">Resumen 2</div>
+
+
+
+
+
+
+
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/D9sdP7nuCcU"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+
+
+
+
+
+
+
+                <br>
+
+                <div class="d-grid gap-2 col-6 mx-auto ">
+                    <input type="submit" class="w-100 btn btn-lg btn-primary" name="enviar" value="Continuar"><br>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</main>
+<?php 
+	require('templates/footer.php');
+?>
